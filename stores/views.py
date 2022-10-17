e50ade45c02bc5ac1099a72e37b060238a6b5533
mohammad <malhumaidaan@gmail.com>
2022-10-17 11:31:15 +0300

    Create Form Done

--- stores/views.py
+++ stores/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from stores import models
+from stores.forms import StoreItemForm
 
 
 def get_store_items(request: HttpRequest) -> HttpResponse:
@@ -10,3 +11,19 @@ def get_store_items(request: HttpRequest) -> HttpResponse:
         "store_items": store_items,
     }
     return render(request, "store_item_list.html", context)
+
+
+def create_store_item(request):
+    create_item = StoreItemForm()
+    if request.method == "POST":
+        _create_item = StoreItemForm(request.POST)
+        if _create_item.is_valid():
+            _create_item.save()
+            return redirect("store-item-list")
+
+
+    context = {
+        "create_item": create_item,
+    }
+    return render(request, "create_store_item.html", context)
+
