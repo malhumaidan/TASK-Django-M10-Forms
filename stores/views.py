ea3186df78634ebc1ee39aabe68d8aa00efc3153
mohammad <malhumaidaan@gmail.com>
2022-10-17 13:20:31 +0300

    Update View Done with edit button in card

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
@@ -26,4 +27,21 @@ def create_store_item(request):
         "create_item": create_item,
     }
     return render(request, "create_store_item.html", context)
+
+
+def update_store_item(request, item_id):
+    store_item = models.StoreItem.objects.get(id=item_id)
+    form = StoreItemForm(instance=store_item)
+    if request.method == "POST":
+        _store_item = StoreItemForm(request.POST, instance=store_item)
+        if _store_item.is_valid():
+            _store_item.save()
+            return redirect("store-item-list")
+
+
+    context = {
+        "store_item": store_item,
+        "form": form,
+    }
+    return render(request, "update_store_item.html", context)
 
